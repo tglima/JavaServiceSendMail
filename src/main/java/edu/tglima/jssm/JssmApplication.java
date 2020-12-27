ac29783ce5d907f6998cab68b82254dd60424277
tglima <thg.limadesousa@gmail.com>
2020-12-27 19:50:20 -0300

    Cria a estrutura base do projeto com as dependências

---
+++ src/main/java/edu/tglima/jssm/JssmApplication.java
@@ -0,0 +1,13 @@
+package edu.tglima.jssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JssmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JssmApplication.class, args);
+	}
+
+}
