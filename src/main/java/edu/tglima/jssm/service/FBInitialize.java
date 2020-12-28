a6a8c9acdb9867c53f78c3b06d13adbad451e6a4
tglima <thg.limadesousa@gmail.com>
2020-12-27 22:47:05 -0300

    Adiciona o arquivo FBInitialize responsável por carregar as configurações de inicialização do firebase

--- src/main/java/edu/tglima/jssm/service/FBInitialize.java
+++ src/main/java/edu/tglima/jssm/service/FBInitialize.java
@@ -1,2 +1,42 @@
-package edu.tglima.jssm.service;public class FBInitialize {
+package edu.tglima.jssm.service;
+
+
+import edu.tglima.jssm.helper.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.auth.oauth2.GoogleCredentials;
+
+@Service
+public class FBInitialize
+{
+    @Autowired
+    ResourceLoader resourceLoader;
+    private final Logger logger = LoggerFactory.getLogger(FBInitialize.class);
+
+    @PostConstruct
+    public void initialize()
+    {
+        try
+        {
+            Resource resource = resourceLoader.getResource(Constant.PATH_FILE_GOOGLE_ACCOUNT_KEY);
+            FirebaseOptions firebaseOptions =  FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
+                    .setDatabaseUrl(Constant.URL_DB_FIREBASE)
+                    .build();
+
+            FirebaseApp.initializeApp(firebaseOptions);
+        }
+        catch (Exception ex)
+        {
+            logger.error(ex.getMessage());
+        }
+    }
+
 }
